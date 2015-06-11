7cfc1cc87cfada434c31efd468417b9c9a8783c9
freedom0036 <freedom.zeta578@gmail.com>
2015-06-11 20:03:07 +0700

    Gif บังคับให้เอาขึ้นไม่รู้ทำไม

--- ticketApp/src/th/go/ticket/app/enjoy/form/SeatingDetailForm.java
+++ ticketApp/src/th/go/ticket/app/enjoy/form/SeatingDetailForm.java
@@ -6,89 +6,128 @@ import java.util.List;
  
 
 
+
 import th.go.ticket.app.enjoy.bean.BookingTypeBean;
+import th.go.ticket.app.enjoy.bean.FieldZoneDetailBean;
 import th.go.ticket.app.enjoy.bean.FieldzonemasterBean;
 import th.go.ticket.app.enjoy.bean.SeatingDetailBean;
 
-public class SeatingDetailForm {
+public class SeatingDetailForm {  
+	List<FieldzonemasterBean>         zoneMasterList;
+	FieldzonemasterBean               fieldzonemasterBean;
+	List<FieldZoneDetailBean>         fieldZoneDetailBeans;
+	String                            fieldzonemasterName;
+	int 							  fieldZoneId;
+	SeatingDetailBean                 seatingDetailBean;
+	BookingTypeBean                   bookingTypeBean;
+	String                            userLevel; 
+
 	
-	List<SeatingDetailBean> 		seatingDetailBeans; 
-	List<FieldzonemasterBean>       fieldzonemasterBeans;
-	String                          fieldzonemasterName;
-	SeatingDetailBean               seatingDetailBean;
-	BookingTypeBean                 bookingTypeBean;
-	String                          userLevel; 
-	
-	public SeatingDetailForm(){
-		this.seatingDetailBeans     = new ArrayList<SeatingDetailBean>(); 
-		this.fieldzonemasterBeans   = new ArrayList<FieldzonemasterBean>();
+	public SeatingDetailForm(){ 
+		this.zoneMasterList         = new ArrayList<FieldzonemasterBean>();
         this.seatingDetailBean      = new SeatingDetailBean(); 
+        this.fieldZoneDetailBeans   = new ArrayList<FieldZoneDetailBean>();
         this.bookingTypeBean        = new BookingTypeBean();
+        this.fieldzonemasterBean    = new FieldzonemasterBean();
         this.userLevel              = "";
+        this.fieldZoneId            = 0;
         
 	}
 
-	public List<SeatingDetailBean> getSeatingDetailBeans() {
-		return seatingDetailBeans;
+
+	public List<FieldzonemasterBean> getZoneMasterList() {
+		return zoneMasterList;
 	}
 
-	public void setSeatingDetailBeans(List<SeatingDetailBean> seatingDetailBeans) {
-		this.seatingDetailBeans = seatingDetailBeans;
-	}
-	   
-	public List<FieldzonemasterBean> getFieldzonemasterBeans() {
-		return fieldzonemasterBeans;
+
+	public void setZoneMasterList(List<FieldzonemasterBean> zoneMasterList) {
+		this.zoneMasterList = zoneMasterList;
 	}
 
-	public void setFieldzonemasterBeans(
-			List<FieldzonemasterBean> fieldzonemasterBeans) {
-		this.fieldzonemasterBeans = fieldzonemasterBeans;
+
+	public FieldzonemasterBean getFieldzonemasterBean() {
+		return fieldzonemasterBean;
 	}
+
+
+	public void setFieldzonemasterBean(FieldzonemasterBean fieldzonemasterBean) {
+		this.fieldzonemasterBean = fieldzonemasterBean;
+	}
+
+
+
+
+	public List<FieldZoneDetailBean> getFieldZoneDetailBeans() {
+		return fieldZoneDetailBeans;
+	}
+
+
+	public void setFieldZoneDetailBeans(List<FieldZoneDetailBean> fieldZoneDetailBeans) {
+		this.fieldZoneDetailBeans = fieldZoneDetailBeans;
+	}
+
 
 	public String getFieldzonemasterName() {
 		return fieldzonemasterName;
 	}
 
+
 	public void setFieldzonemasterName(String fieldzonemasterName) {
 		this.fieldzonemasterName = fieldzonemasterName;
 	}
 
-	
+
+	public int getFieldZoneId() {
+		return fieldZoneId;
+	}
+
+
+	public void setFieldZoneId(int fieldZoneId) {
+		this.fieldZoneId = fieldZoneId;
+	}
+
+
 	public SeatingDetailBean getSeatingDetailBean() {
 		return seatingDetailBean;
 	}
 
+
 	public void setSeatingDetailBean(SeatingDetailBean seatingDetailBean) {
 		this.seatingDetailBean = seatingDetailBean;
 	}
-	  
+
+
 	public BookingTypeBean getBookingTypeBean() {
 		return bookingTypeBean;
 	}
 
+
 	public void setBookingTypeBean(BookingTypeBean bookingTypeBean) {
 		this.bookingTypeBean = bookingTypeBean;
 	}
-	
-	
+
 
 	public String getUserLevel() {
 		return userLevel;
 	}
 
+
 	public void setUserLevel(String userLevel) {
 		this.userLevel = userLevel;
 	}
 
+
 	@Override
 	public String toString() {
-		return "SeatingDetailForm [seatingDetailBeans=" + seatingDetailBeans
-				+ ", fieldzonemasterBeans=" + fieldzonemasterBeans
+		return "SeatingDetailForm [zoneMasterList=" + zoneMasterList
+				+ ", fieldzonemasterBean=" + fieldzonemasterBean
+				+ ", fieldZoneDetailBeans=" + fieldZoneDetailBeans
 				+ ", fieldzonemasterName=" + fieldzonemasterName
-				+ ", seatingDetailBean=" + seatingDetailBean
-				+ ", bookingTypeBean=" + bookingTypeBean + "]";
+				+ ", fieldZoneId=" + fieldZoneId + ", seatingDetailBean="
+				+ seatingDetailBean + ", bookingTypeBean=" + bookingTypeBean
+				+ ", userLevel=" + userLevel + "]";
 	}
-
+ 
 	 
 	  
 }
